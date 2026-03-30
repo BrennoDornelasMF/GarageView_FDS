58008c0632a7f0c6aa0395b0dd3ac8dce47de2a2
JoãoViniciusZ <joaovrdfreitas@gmail.com>
2026-03-30 20:06:21 -0300

    fix: recriar arquivos com encoding UTF-8 correto

--- forum/models.py
+++ forum/models.py
@@ -1,16 +1,15 @@
-from django.db import models
-
+﻿from django.db import models
 import datetime
 from django.utils import timezone
+
+
 class Anuncio(models.Model):
     titulo = models.CharField(max_length=200)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     imagem_url = models.URLField(blank=True, null=True)
-    vendedor = models.CharField(max_length=200, default="anônimo")
-    data_criacao = models.DateTimeField("Criado em ", auto_now_add=True)
+    vendedor = models.CharField(max_length=200, default='anônimo')
+    data_criacao = models.DateTimeField('Criado em ', auto_now_add=True)
 
     def __str__(self):
-        return f"[{self.id}] {self.titulo} - {self.vendedor}"
-
-    
+        return f'[{self.id}] {self.titulo} - {self.vendedor}'
--- forum/urls.py
+++ forum/urls.py
@@ -1,15 +1,12 @@
-from django.urls import path
+﻿from django.urls import path
 from . import views
 
 app_name = 'forum'
 urlpatterns = [
-    # ex: /forum/
-    path("", views.MainView.as_view(), name="index"),
-    path("ad/<int:ad_id>/", views.AdDetailView.as_view(), name="ad_detail"),
-    path("ad/inserir/", views.CreateAdView.as_view(), name="ad_create"),
-    path("meus-anuncios/", views.SellerAdsView.as_view(), name="seller_ads"),
-    path("ad/<int:ad_id>/editar/", views.EditAdView.as_view(), name="ad_edit"),
-    path("ad/<int:ad_id>/excluir/", views.DeleteAdView.as_view(), name="ad_delete"),
-    
-    
+    path('', views.MainView.as_view(), name='index'),
+    path('ad/<int:ad_id>/', views.AdDetailView.as_view(), name='ad_detail'),
+    path('ad/inserir/', views.CreateAdView.as_view(), name='ad_create'),
+    path('meus-anuncios/', views.SellerAdsView.as_view(), name='seller_ads'),
+    path('ad/<int:ad_id>/editar/', views.EditAdView.as_view(), name='ad_edit'),
+    path('ad/<int:ad_id>/excluir/', views.DeleteAdView.as_view(), name='ad_delete'),
 ]
